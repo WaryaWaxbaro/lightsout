9c84f66918613568216b342a2ccf4205ab99ee89
Warya Waxbaro <warya.waxbaro@gmail.com>
2019-08-27 16:46:32 +0300

    Updates

---
+++ precache-manifest.8ab4b69207f153a2cd3daf276088263c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af1ec328f35c9319cdacacb0cc89d1a",
+    "url": "/lightsout/index.html"
+  },
+  {
+    "revision": "22bb52df24609c32ceb1",
+    "url": "/lightsout/static/css/main.fde38598.chunk.css"
+  },
+  {
+    "revision": "c5e2e0aefeabc12d8a1b",
+    "url": "/lightsout/static/js/2.d245da0a.chunk.js"
+  },
+  {
+    "revision": "22bb52df24609c32ceb1",
+    "url": "/lightsout/static/js/main.64e93db3.chunk.js"
+  },
+  {
+    "revision": "050db2e93749387dd29e",
+    "url": "/lightsout/static/js/runtime~main.8e2101e2.js"
+  }
+]);
